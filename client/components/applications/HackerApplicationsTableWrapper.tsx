ea157c3f77868b3a510d10e824ec26d04c1b4650
Alice <alice.zewei.zhao@gmail.com>
2026-08-20 17:51:23 -0400

    restructure pulling from db

--- client/components/applications/HackerApplicationsTableWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import { getHackerApplications } from '@/lib/data/hackers'
-import { HackerApplicationsTable } from '../HackerApplicationsTable'
-
-export async function HackerApplicationsTableWrapper() {
-  const applications = await getHackerApplications()
-  return <HackerApplicationsTable applications={applications} />
-}
